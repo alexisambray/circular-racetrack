70d7f4873bb060961095e8bba33f98681523766e
Alexis Ambray <alexisambray@gmail.com>
2021-04-30 19:31:19 +0800

    Final solution to circular racetrack

--- circular-racetrack.c
+++ circular-racetrack.c
@@ -1,31 +1,49 @@
 #include <stdio.h>
 
-int GetCarTrack(int time) {
-   if (time <= 30) {
-    puts("Brian's car in Concrete.");
-  } else if (time > 30 && time <= 85) {
-    puts("Brian's car in Mud.");
-  } else if (time > 85 && time <= 132) {
-    puts("Brian's car in Sand.");
-  } else if (time > 132 && time <= 170) {
-    puts("Brian's car in Asphalt");
+typedef enum { CONCRETE = 30, MUD = 55, SAND = 47, ASPHALT = 38 } CarTrack;
+const int SECONDS_IN_REV = CONCRETE + MUD + SAND + ASPHALT;
+
+CarTrack getCarTrack(int time) {
+  CarTrack carTrack;
+
+  if (time < CONCRETE) {
+    carTrack = CONCRETE;
+  } else if (time < CONCRETE + MUD) {
+    carTrack = MUD;
+  } else if (time < CONCRETE + MUD + SAND) {
+    carTrack = SAND;
+  } else if (time < CONCRETE + MUD + SAND + ASPHALT) {
+    carTrack = ASPHALT;
+  }
+  return carTrack;
+}
+
+void displayCarTrack(CarTrack carTrack) {
+  switch (carTrack) {
+    case CONCRETE:
+      puts("Sly Slick's car is in concrete.");
+      break;
+    case MUD:
+      puts("Sly Slick's car is in mud.");
+      break;
+    case SAND:
+      puts("Sly Slick's car is in sand.");
+      break;
+    case ASPHALT:
+      puts("Sly Slick's car is in asphalt.");
   }
 }
 
 int main() {
   int time = 0;
-  int tracks = 0;
-  int lastRound = 0;
-  int circle = 30 + 55 + 47 + 38;  // Total time
 
   printf("Enter positive TIME value in seconds: ");
   scanf("%d", &time);
 
-  if (time <= circle) {
-    GetCarTrack(time);
-  } else {
-    tracks = (time / circle);
-    GetCarTrack(lastRound);
-  }
+  time %= SECONDS_IN_REV;
+
+  CarTrack carTrack = getCarTrack(time);
+  displayCarTrack(carTrack);
+
   return 0;
 }
